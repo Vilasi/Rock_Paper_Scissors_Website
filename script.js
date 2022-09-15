6a99d656bb09cde22c9c3f962d6fc17f7c33d9b9
Vilasi <VilasiCoding@gmail.com>
2022-09-14 23:12:07 -0400

    Added win/loss/tie message. Cleaned up variable declaration order

--- script.js
+++ script.js
@@ -1,53 +1,57 @@
-// Chooses the computer's hand based on a randomly chosen number.
-let computerSelection = '';
-function computerChoice() {
-  let selectionArray = ['Rock', 'Paper', 'Scissors'];
-  return (computerSelection = selectionArray[Math.floor(Math.random() * 3)]);
-}
+let scoreBox = document.getElementById('score-box');
+let domRound = document.getElementById('round');
+let domComputerChoice = document.getElementById('computerSelection');
+let domUserScore = document.getElementById('domUserScore');
+let domComputerScore = document.getElementById('domComputerScore');
+let domUserChoice = document.getElementById('userChoice');
+let roundResultsMessage = document.getElementById('tie');
 
 // Compares the computer's score to the player's score.
 let computerScore = 0;
 let userScore = 0;
 let currentRound = 0;
-let tieDetector = '';
+
+// Chooses the computer's hand based on a randomly chosen number.
+let computerSelection = '';
+
+function computerChoice() {
+  let selectionArray = ['Rock', 'Paper', 'Scissors'];
+  return (computerSelection = selectionArray[Math.floor(Math.random() * 3)]);
+}
+
 function playRound(computerSelection, userSelection) {
   if (
     (userSelection === 'Paper' && computerSelection === 'Rock') ||
     (userSelection === 'Rock' && computerSelection === 'Scissors') ||
     (userSelection === 'Scissors' && computerSelection === 'Paper')
   ) {
+    roundResultsMessage.textContent = 'You WON!';
+    roundResultsMessage.style.visibility = 'visible';
+    roundResultsMessage.style.color = '#276FBF';
     userScore += 1;
-    tieDetector = false;
     currentRound += 1;
+    return currentRound;
   } else if (userSelection === computerSelection) {
-    tieDetector = true;
+    roundResultsMessage.textContent = 'TIE!';
+    roundResultsMessage.style.visibility = 'visible';
+    roundResultsMessage.style.color = 'grey';
     currentRound += 1;
+    return currentRound;
   } else {
+    roundResultsMessage.textContent = 'You LOST!';
+    roundResultsMessage.style.visibility = 'visible';
+    roundResultsMessage.style.color = 'red';
     computerScore += 1;
-    tieDetector = false;
     currentRound += 1;
+    return currentRound;
   }
 }
 
-// Gets the div that holds the score.
-let scoreBox = document.getElementById('score-box');
-
-// Displays the current round.
-let domRound = document.getElementById('round');
-
-// Displays what the computer chooses.
-let domCompChoice = document.getElementById('computerSelection');
-
-// To display the user's score.
-let domUserScore = document.getElementById('domUserScore');
-
-// To display the computer's score.
-let domComputerScore = document.getElementById('domComputerScore');
-
-// To display the tie message.
-let tieParagraph = document.getElementById('tie');
-tieParagraph.style.visibility = 'hidden';
-
+roundResultsMessage.style.visibility = 'hidden';
+if (!currentRound) {
+  domComputerChoice.style.visibility = 'hidden';
+  domUserChoice.style.visibility = 'hidden';
+}
 let userSelection = '';
 const buttons = document.querySelectorAll('span');
 buttons.forEach((btn) => {
@@ -55,14 +59,14 @@ buttons.forEach((btn) => {
     userSelection = x.target.innerText;
     computerChoice();
 
-    domCompChoice.textContent = 'Computer Chooses:' + ` ${computerSelection}`;
-
     playRound(computerSelection, userSelection);
-    if (tieDetector) {
-      tieParagraph.style.visibility = 'visible';
-    } else {
-      tieParagraph.style.visibility = 'hidden';
+
+    if (currentRound) {
+      domComputerChoice.style.visibility = 'visible';
+      domUserChoice.style.visibility = 'visible';
     }
+    domUserChoice.textContent = `You Chose: ${userSelection}`;
+    domComputerChoice.textContent = `Computer Chooses: ${computerSelection}`;
     domRound.textContent = `Round: ${currentRound}`;
     domUserScore.textContent = `Your Score: ${userScore}`;
     domComputerScore.textContent = `Computer Score: ${computerScore}`;
